43bb11396f56256a858b4ceda7b646044099cbb8
TenzDelek <tibetdelek@gmail.com>
2024-06-05 21:55:59 +0530

    further changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import Card from "./components/cards/Card"
 import Skeleton from "./components/cards/Skeleton";
-import data from "./json/Data.json"
 import { useEffect, useState } from "react";
+import image1 from './assets/image/sample_02.jpg';
+import image2 from './assets/image/sample_03.jpg';
+import image3 from './assets/image/sample_04.jpg';
+import image4 from './assets/image/sample_05.jpg';
+
+const data = [
+  {
+    "title": "JJKS1",
+    "img": image1,
+    "motto": "wake up"
+  },
+  {
+    "title": "JJKS2",
+    "tag": "Hero",
+    "img": image2,
+    "motto": "slash and burn"
+  },
+  {
+    "title": "berserker",
+    "img": image3,
+    "motto": "All shall end"
+  },
+  {
+    "title": "AOT",
+    "img": image4,
+    "motto": "tatake"
+  }
+];
+
 function App() {
 
   const [isloading, setloading] = useState(true);
@@ -13,6 +41,7 @@ function App() {
 
   return (
     <div className=" h-screen w-fit flex items-center justify-center pl-2">
+   
     <div className=' flex gap-2'>
       {[...data,...data,...data,...data].map((item,index)=>(
         <div key={index}>
--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { GiMountaintop } from "react-icons/gi";
+import image1 from '../../assets/image/sample_02.jpg'
 
 const Card = ({props}) => {
  
   return (
     <div className=' h-80 w-52 border-[1px] rounded-md space-y-2 p-2 bg-white shadow-lg transition hover:shadow-none '>
-        <div className='  overflow-hidden relative'>
+        <div className=' overflow-hidden relative'>
         
         <img src={props.img}  className=' hover:scale-[1.1] transition h-56 w-48 rounded-sm grayscale object-cover' />
        {props.tag && <div className=' absolute top-0 right-0  bg-green-500 text-white px-2 py-1  rounded-full text-[10px]'>
