ad82739c8326d84f83488d5ca5cc0b93321afec0
TenzDelek <tibetdelek@gmail.com>
2024-06-05 20:06:40 +0530

    implemented json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Card from "./components/cards/Card"
-
+import data from "./json/Data.json"
 function App() {
 
 
   return (
-    <div className=" h-screen w-full flex items-center justify-center">
-    <div className=' font-custom'>
-      <Card/>
+    <div className=" h-screen w-full flex items-center justify-center pl-20">
+    <div className=' font-custom flex gap-2'>
+      {[...data,...data,...data,...data,...data].map((item,index)=>(
+        <div key={index}>
+           <Card props={item}/>
+           </div>
+      ))}
+     
     </div>
      
     </div>
--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { GiMountaintop } from "react-icons/gi";
-const Card = () => {
+const Card = ({props}) => {
   return (
     <div className=' w-40 border-[1px] rounded-md space-y-2 p-2 bg-white shadow-lg transition hover:shadow-none '>
-        <div className=' relative'>
-        <img src='../public/image/sample_01.jpg' className=' rounded-sm grayscale' />
+        <div className=' bg-slate-500 relative'>
+        
+        <img src={props.img} className=' rounded-sm grayscale' />
         <div className=' absolute top-0 right-0 bg-black text-white px-2 py-1  rounded-full text-[10px]'>
-            Tenzin
+            {props.tag}
         </div>
         </div>
         
         <div className=' flex justify-between items-center'>
-        <p className=' text-xs font-bold'># Mountain trek</p>
+        <p className=' text-xs font-bold'># {props.title}</p>
         <GiMountaintop /></div>
     </div>
   )
